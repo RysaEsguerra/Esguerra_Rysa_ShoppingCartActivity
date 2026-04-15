c2321ad2506c38bb2374db9c1afc53d9f51a9561
RysaEsguerra <decenarysa0001@gmail.com>
2026-04-15 22:42:18 +0800

    add menu display

--- Project.cs
+++ Project.cs
@@ -13,4 +13,18 @@ class Product
     public string Name;
     public double Price;
     public int Stock;
+    public void Display()
+    {
+        Console.WriteLine($"{Id}. {Name} - ₱{Price} (Stock: {Stock})");
+    }
 }
+class Program
+{
+    static void Main()
+    {
+        Product p1 = new Product { Id = 1, Name = "Watermelon", Price = 70, Stock = 10 };
+        Product p2 = new Product { Id = 2, Name = "Peach", Price = 15, Stock = 20 };
+        Product p3 = new Product { Id = 3, Name = "Melon", Price = 50, Stock = 5 };
+
+        string nextCustomer = "Y";
+
